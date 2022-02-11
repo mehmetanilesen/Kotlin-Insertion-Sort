aede291e5ea7ef256c912420e6f8b68807ddbc17
mehmetanilesen <mehmetanilesen@gmail.com>
2022-02-11 17:12:54 +0300

    Insertion Sort With Kotlin

---
+++ app/src/main/java/com/example/insertionsort/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.insertionsort
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var x = InsertionSort(arrayListOf(22,27,10,5,3,9,1,18))
+        println(arrayListOf(22,27,10,5,3,9,1,18))
+        println(x.Sort())
+    }
+}
+
+class InsertionSort(var insertList : ArrayList<Int>)
+{
+    fun Sort() : ArrayList<Int>
+    {
+        var temp : Int = 0
+        var position: Int = 0
+
+        for (i in 1 until insertList.size)
+        {
+            position = i
+            temp = insertList[i]
+
+            for ( j in i  downTo 0)
+            {
+                if (temp < insertList[j])
+                {
+                    insertList[position] = insertList[j]
+                    insertList[j] = temp
+                    position--
+                }
+            }
+
+        }
+        return insertList
+    }
+}
